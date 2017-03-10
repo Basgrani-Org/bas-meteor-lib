f9f3220a49992cdb25e095812204637cafb9836f
Jorge <jorge@basgrani.com>
2017-03-10 16:55:15 +0100

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,10 @@ module.exports = function ( grunt ) {
             name_root_file: '<%= pkg.name %>',
             utils_dist_dir: 'utils/dist',
             utils_es6_dir: 'utils/es6',
+            facebook_login_dist_dir: 'facebook-login/dist',
+            facebook_login_es6_dir: 'facebook-login/es6',
+            ip_geo_dist_dir: 'ip-geo/dist',
+            ip_geo_es6_dir: 'ip-geo/es6',
             jwplayer_dist_dir: 'jwplayer/dist',
             jwplayer_es6_dir: 'jwplayer/es6'
         },
@@ -49,6 +53,26 @@ module.exports = function ( grunt ) {
                 dest: '<%= meta.utils_dist_dir %>/'
 
             },
+            facebook_login: {
+                options: {
+                    sourceMap: false
+                },
+                expand: true,
+                cwd: '<%= meta.facebook_login_es6_dir %>/',
+                src: '**/*.js',
+                dest: '<%= meta.facebook_login_dist_dir %>/'
+
+            },
+            ip_geo: {
+                options: {
+                    sourceMap: false
+                },
+                expand: true,
+                cwd: '<%= meta.ip_geo_es6_dir %>/',
+                src: '**/*.js',
+                dest: '<%= meta.ip_geo_dist_dir %>/'
+
+            },
             jwplayer: {
                 options: {
                     sourceMap: false
@@ -69,6 +93,12 @@ module.exports = function ( grunt ) {
         clean: {
             utils: {
                 src: ["<%= meta.utils_dist_dir %>/*"]
+            },
+            facebook_login: {
+                src: ["<%= meta.facebook_login_dist_dir %>/*"]
+            },
+            ip_geo: {
+                src: ["<%= meta.ip_geo_dist_dir %>/*"]
             },
             jwplayer: {
                 src: ["<%= meta.jwplayer_dist_dir %>/*"]
@@ -104,6 +134,14 @@ module.exports = function ( grunt ) {
     // Babel (utils)
     grunt.registerTask('_utils_babel_compile', ['clean:utils', 'babel:utils']);
     grunt.registerTask('utils_babel_compile', ['_utils_babel_compile', 'notify:js_compile']);
+
+    // Babel (facebook_login)
+    grunt.registerTask('_facebook_login_babel_compile', ['clean:facebook_login', 'babel:facebook_login']);
+    grunt.registerTask('facebook_login_babel_compile', ['_facebook_login_babel_compile', 'notify:js_compile']);
+
+    // Babel (ip_geo)
+    grunt.registerTask('_ip_geo_babel_compile', ['clean:ip_geo', 'babel:ip_geo']);
+    grunt.registerTask('ip_geo_babel_compile', ['_ip_geo_babel_compile', 'notify:js_compile']);
 
     // Babel (jwplayer)
     grunt.registerTask('_jwplayer_babel_compile', ['clean:jwplayer', 'babel:jwplayer']);
